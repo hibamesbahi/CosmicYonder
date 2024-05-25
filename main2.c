f6dd63889247d2b541a587acb469daeb3490e7cc
bina0413 <binetamhdiop@gmail.com>
2024-05-25 18:19:22 +0200

    Create main2.c

---
+++ main2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>                //y->largeur   x->longueur
+#include <stdlib.h>
+#include <ncurses.h>
+#include <time.h>
+#include <stdbool.h>
+#include "declaration.h"
+
+const char mur='#';
+const char porte='/';
+const char vide= ' ';
+int nb_salle=0;
+int compteur_porte=5;
+int relative_X=0;
+int relative_Y=0;
+extern int graine;
+
+
+
+
+menu(){
+//intro
+//sauvgarde -
+
+
+  //nouvelle partie
+  //graine
+
+
+
+
+
+}
+
+
+niveau1(){
+
+  niveau2();
+}
+
+int main(){
+  srand(50); 
+  initscr();
+  menu();//-demande la graine
+  //timer
+  start_color();  //activer les couleur
+  init_pair(1,VIOLET,0);
+  init_pair(2,6,0);
+  init_color(VIOLET, 250,25,255);
+  init_pair(3,7,VIOLET);
+
+  Salle* map=NULL;
+  map =createurMap(&nb_salle);
+
+
+  decouverte = (bool**)malloc(sizeof(bool)*LINES);
+  for(int i=0; i<LINES; i++)
+  {
+    decouverte[i]= (bool*)malloc(sizeof(bool)*COLS);
+    for (int j=0; j<= COLS; j++) 
+      decouverte[i][j]=0;
+  }
+  
+  contour();
+  //refresh();
+  noecho();
+  curs_set(0);
+  
+  mouvementPerso(map);
+  
+  // afficher defaite victoire
+  getch();
+  free(decouverte);
+  free(map);
+  endwin();
+
+  return 0;
+}
