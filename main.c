4866feb1b3fb99593f921eb564c4b9a62737e93b
bina0413 <binetamhdiop@gmail.com>
2024-05-26 23:31:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -13,49 +13,34 @@ int compteur_porte=5;
 int relative_X=0;
 int relative_Y=0;
 int graine;
+char nom[16];
+int experience=0;
+clock_t temps=0;
+int indice;
+clock_t debut;
+clock_t fin;
 
-bool **decouverte;
+Salle *map=NULL;
+
+//bool **decouverte;
 Monstre monstre;
 
 
 
 
 
-int main(){
-  srand(50); 
+int main(){ 
   initscr();
-  //menu();//-demande la graine
   //timer
   start_color();  //activer les couleur
   init_pair(1,VIOLET,0);
   init_pair(2,6,0);
   init_color(VIOLET, 250,25,255);
   init_pair(3,7,VIOLET);
-  printw("**************entrez la graine\n");
-  scanw("%d",&graine);
-  
-  Salle* map=NULL;
-  map =createurMap(&nb_salle);
-
-
-  decouverte = (bool**)malloc(sizeof(bool*)*LINES);
-  for(int i=0; i<LINES; i++)
-  {
-    decouverte[i]= (bool*)malloc(sizeof(bool)*COLS);
-    for (int j=0; j<= COLS; j++) 
-      decouverte[i][j]=0;
-  }
-  
-  contour();
-  refresh();
-  noecho();
   curs_set(0);
-  
-  mouvementPerso(map);
-  // afficher defaite victoire
-  getch();
-  free(decouverte);
-  free(map);
+  Menu();
+  refresh();
+
   endwin();
 
   return 0;
